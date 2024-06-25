6de6d8d08f2b9fbffd8470995d01d9f7baf88f4e
Mariano Vallés <mariano.valles@wooga.net>
2024-06-25 13:25:39 +0200

    Add 200 response for get_transacion_info test

--- test/app_store/api/transaction_info_test.exs
+++ test/app_store/api/transaction_info_test.exs
@@ -4,7 +4,27 @@ defmodule AppStore.API.TransactionInfoTest do
   alias AppStore.API.TransactionInfo
 
   describe "get_transaction_info/3" do
-    test "Get info for a specific transaction", %{bypass: bypass, app_store: app_store} do
+    test "returns 200 and an expected response body", %{bypass: bypass, app_store: app_store} do
+      expected_body = %{signedTransactionInfo: "signed_transaction_info_value"} |> Jason.encode!()
+
+      Bypass.expect_once(bypass, "GET", "/inApps/v1/transactions/1234", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("server", "daiquiri/3.0.0")
+        |> Plug.Conn.resp(200, expected_body)
+      end)
+
+      {:ok, %AppStore.API.Response{body: body, status: status}} =
+        TransactionInfo.get_transaction_info(
+          app_store.api_config,
+          "token",
+          "1234"
+        )
+
+      assert status === 200
+      assert body === expected_body
+    end
+
+    test "returns 401 for unauthenticated request", %{bypass: bypass, app_store: app_store} do
       Bypass.expect_once(bypass, "GET", "/inApps/v1/transactions/transaction-id", fn conn ->
         conn
         |> Plug.Conn.put_resp_header("server", "daiquiri/3.0.0")
